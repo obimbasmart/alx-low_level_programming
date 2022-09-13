134fee25964e06707c88543ea9bac2cc7f1c0a18
Obimba Smart <obimbasmartchukwunenye@gmail.com>
2022-09-14 00:22:29 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,16 +4,16 @@
  * Return: int
  */
 int main(void)
-{ 
+{
 char theChar;
 int theInt;
 long int longInt;
 long long int longLongInt;
 float thefloat;
 printf("Size of a char: %d byte(s)\n", sizeof(theChar));
-printf("Size of an int: %d byte(s)\n", sizeof(theInt));	 
+printf("Size of an int: %d byte(s)\n", sizeof(theInt));
 printf("Size of a long int: %ld byte(s)\n", sizeof(longInt));
-printf("Size of a long long int: %d byte(s)\n", sizeof(longLongInt));	 
+printf("Size of a long long int: %d byte(s)\n", sizeof(longLongInt));
 printf("Size of a float: %d byte(s)\n", sizeof(thefloat));
-return (0);  
+return (0);
 }
