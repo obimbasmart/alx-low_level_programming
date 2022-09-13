b68c65ab58924b991b7a2dd6fd12031af95b6b23
Obimba Smart <obimbasmartchukwunenye@gmail.com>
2022-09-14 00:17:13 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- * main: driver function
- * return: 0
+ * main - driver function
+ * Return: 0
  */
 int main()  
-{
-  
+{ 
 char theChar;
 int theInt;
 long int longInt;
@@ -17,7 +15,5 @@ printf("Size of an int: %d byte(s)\n", sizeof(theInt));
 printf("Size of a long int: %ld byte(s)\n", sizeof(longInt));
 printf("Size of a long long int: %d byte(s)\n", sizeof(longLongInt));	 
 printf("Size of a float: %d byte(s)\n", sizeof(thefloat));
-
-return (0);
-    
+return (0);  
 }
