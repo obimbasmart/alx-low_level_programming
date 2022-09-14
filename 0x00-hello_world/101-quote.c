7a745665e1faa6586ffa58ced6a5ff761325520b
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-13 17:27:18 -0700

    updated file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main: driver function: write to stderr
+ * main - driver function: write to stderr
  * Return: 0
  */
-int main()
+int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
