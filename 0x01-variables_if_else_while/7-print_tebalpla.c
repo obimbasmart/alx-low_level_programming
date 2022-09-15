881c7d56c534de71e58f188ca803e8b440ed3165
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-15 14:32:43 -0700

    added a new file

---
+++ 0x01-variables_if_else_while/7-print_tebalpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - driver function
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+char alphas[] = "abcdefghijklmnopqrstuvwxyz";
+for (i = 25; i >= 0; i--)
+{
+putchar(alphas[i]);
+}
+putchar('\n');
+return (0);
+}
