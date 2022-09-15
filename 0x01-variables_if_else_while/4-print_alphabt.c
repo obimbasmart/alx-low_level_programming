1869d3bdd315acffab6c80eed07e450ea160b805
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-15 13:21:13 -0700

    added a new file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - driver function
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+char alphas[] = "abcdefghijklmnopqrstuvwxyz";
+for (i = 0; i <= 25; i++)
+{
+if (alphas[i] == 'q' || alphas[i] == 'e')
+{
+continue;
+}
+putchar(alphas[i]);
+}
+putchar('\n');
+return (0);
+}
