4514875b7b3cbded93bfc6418fa30dad8dbfb093
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-15 16:20:53 -0700

    added a new file

---
+++ 0x01-variables_if_else_while/100-print_com3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/*
+ * main - driver function
+ * prints all possible combinations of single-digit numbers.
+ * return: 0
+ */
+int main(void)
+{
+int i;
+int j;
+for (i = 48; i <= 57; i++)
+{
+for (j = i + 1; j <= 57; j++)
+{
+putchar(i);
+putchar(j);
+if (i == 56 && j == 57)
+{
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
