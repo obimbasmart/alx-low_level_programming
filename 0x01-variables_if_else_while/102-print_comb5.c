e15fb6d76b5ad17d88bcc023657dde75a3c0123d
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-16 01:45:20 -0700

    added a new file

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - driver function
+ * prints all possible combinations of single-digit numbers.
+ * Return: 0
+ */
+int main(void)
+{
+int number1;
+int number2;
+for (number1 = 0; number1 <= 98; number1++)
+{
+for (number2 = number1 + 1; number2 <= 99; number2++)
+{
+putchar((number1 / 10) + '0');
+putchar((number1 % 10) + '0');
+putchar(' ');
+putchar((number2 / 10) + '0');
+putchar((number2 % 10) + '0');
+if (number1 == 98 && number2 == 99)
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+}
+}
