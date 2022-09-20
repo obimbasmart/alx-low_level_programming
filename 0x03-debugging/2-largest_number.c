c632d1a65b93081a6c4f26daa88600768b8c8670
Obimba Smart <obimbasmartchukwunenye@gmail.com>
2022-09-20 06:38:53 +0100

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -3,17 +3,17 @@
 /**
 
  * largest_number - returns the largest of 3 numbers
- 
+
  * @a: first integer
- 
+
  * @b: second integer
- 
+
  * @c: third integer
- 
+
  * Return: largest number
- 
+
  */
- 
+
 int largest_number(int a, int b, int c)
 
 {
