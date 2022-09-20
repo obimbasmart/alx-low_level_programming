70416593538b16caeed2b3cd13f47b62a77daae1
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-20 03:39:34 -0700

    added a new file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - print the sign of a number
+ * @num : int character to test
+ * Return: 0 if number is greater than 0,  false < 0
+ */
+int print_sign(int num)
+{
+if (num > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (num == 0)
+{
+_putchar('0');
+return (0)
+}
+ _putchar('-');
+ return (-1);
+}
