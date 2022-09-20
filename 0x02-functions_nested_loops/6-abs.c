b3d1d8814f2e21f04108d50898880c6d8d324199
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-20 03:56:05 -0700

    added new file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - compute the absolute value of a number
+ * @num : number to convert
+ * Return: 0 if false 1 if true
+ */
+int _abs(int num)
+{
+int abs_num;
+if (num < 0)
+{
+num  = num * 1;
+}
+return (num);
+}
