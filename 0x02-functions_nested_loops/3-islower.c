a6e36fba4a82d37c9b9ec02a028f3da4775b7820
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-20 02:42:14 -0700

    added a new file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower - check if a character is in lowercase
+ * Return: 0 if false
+ * Return: 1 if true
+ */
+int _islower(int)
+{
+return ('c' >= 'a' && 'c' <= 'z')
+}
