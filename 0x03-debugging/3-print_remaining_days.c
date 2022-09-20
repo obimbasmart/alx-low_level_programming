eca3b5792864047dc1f26c23c06e7435f5b497c1
Obimba Smart <obimbasmartchukwunenye@gmail.com>
2022-09-20 07:11:07 +0100

    Update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -5,19 +5,12 @@
 
 
 /**
-
  * print_remaining_days - takes a date and prints how many days are
-
  * left in the year, taking leap years into account
-
  * @month: month in number format
-
  * @day: day of month
-
  * @year: year
-
  * Return: void
-
  */
 
 
@@ -59,13 +52,10 @@ printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
 
 else
-
 {
-
+day++;
 printf("Day of the year: %d\n", day);
-
 printf("Remaining days: %d\n", 365 - day);
-
 }
 
 }
