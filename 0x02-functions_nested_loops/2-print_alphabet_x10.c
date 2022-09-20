23647b1cc7bcf2d8455a904445baf67b32aef1c3
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-20 02:30:54 -0700

    added a new file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - print alphabet in lower case
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+int i;
+int j;
+char alphas[] = "abcdefghijklmnopqrstuvwxyz";
+for (j = 0; j < 10; j++)
+{     
+for (i = 0; i <= 25; i++)
+{
+_putchar(alphas[i]);
+}
+putchar('\n');
+}
+}
