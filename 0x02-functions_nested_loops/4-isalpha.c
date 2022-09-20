fbb4836e6a56b3134401965c5eceff10feddad25
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-20 03:07:06 -0700

    added new fil

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isalpha - check if a character is in alphabetic
+ * @character : int character to test
+ * Return: 0 if false 1 if true
+ */
+int _isalpha(int character)
+{
+  return ((character >= 65 && character <= 90) || (character >= 97 && character <= 122));
+}
