a1f183f4a5f2b676536dd5acabcccc5d26342d43
Obimba Smart <obimbasmartchukwunenye@gmail.com>
2022-09-20 06:27:06 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,13 +4,13 @@
  * main -test function that prints if integer is posetive or negative
  * Return: 0
  */
-void posetive_or_negative(int i);
+
 int main(void)
 {
 int i;
  
 i = 0;
-posetive_or_negative(i);
+positive_or_negative(i);
 
 return (0);
 }
