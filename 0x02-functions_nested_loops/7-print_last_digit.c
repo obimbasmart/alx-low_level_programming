322015f9224c622aec3e102f4946b204233c3305
Obimba Smart <obimbasmartchukwunenye@gmail.com>
2022-09-20 12:26:44 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,6 +9,10 @@ int print_last_digit(int num)
 {
 int last_digit;
 last_digit = num % 10;
+if (last_digit < 0)
+{
+last_digit = -1 * last_digit;
+}
 _putchar(last_digit + '0');
 return last_digit;
 }
