d05c7d09ec2bdfe16f50fcffd9b148d3e9c356c7
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-20 00:55:14 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print "putchar" to output stream
+ * Return: 0
+ */
+int main(void)
+{
+print_putchar();
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+# c - Functions, nested loops
