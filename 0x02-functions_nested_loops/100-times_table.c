75fb6fc11a557728aa5465bf222cdacef2095765
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-21 01:14:41 -0700

    added new file

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* times_table - prints the 9 times table starting from 0
+* Return: void
+*/
+void print_times_table(int n)
+{
+if (n > 15 || n < 0)
+{
+return;
+}
+int next_result;
+int i = 0;
+while (i <= n)
+{
+int j = 0;
+while (j <= n)
+{
+int re = i * j;
+next_result = i * (j + 1);
+if (j != n)
+{
+if (next_result < 10)
+{
+printf("%d,   ", re);
+}
+else if (next_result < 100)
+{
+printf("%d,  ", re);
+}
+else
+{
+printf("%d, ", re);
+}
+}
+else
+{
+printf("%d", re);
+}
+j++;
+}
+putchar('\n');
+i++;
+}
+}
