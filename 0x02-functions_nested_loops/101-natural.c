a3b473869db3a7d0ca08e840dae1bcb4362dbc3b
Obimba Smart <obimbasmartchukwunenye@gmail.com>
2022-09-21 09:58:15 +0100

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,7 +2,7 @@
 #include "main.h"
 /**
 * main - computes and prints the sum of all 
-  the multiples of 3 or 5 below 1024 (excluded)
+* the multiples of 3 or 5 below 1024 (excluded)
 * Return: 0
 */
 int main(void)
