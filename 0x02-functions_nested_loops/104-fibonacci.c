45bcddb498ec71a3ec25793d709296649c1238d4
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-21 03:13:15 -0700

    added new file

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - print the first 50 fibonacci numbers
+* Return: 0
+*/
+int main(void)
+{
+unsigned long int next;
+unsigned long int fn;
+unsigned long int fn_1;
+int i;
+fn = 1;
+fn_1 = 2;
+printf("%lu, %lu, ", fn, fn_1);
+for (i = 0; i < 96; i++)
+{
+next = fn + fn_1;
+fn = fn_1;
+fn_1 = next;
+if (i != 47)
+{
+printf("%lu, ", next);
+}
+else
+{
+printf("%lu", next);
+}
+}
+putchar('\n');
+return (0);
+}
