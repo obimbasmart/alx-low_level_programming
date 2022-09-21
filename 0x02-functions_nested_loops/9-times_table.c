4678e74735428e20bebe654febfea5d4bc3919e2
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-20 22:59:53 -0700

    added a new file

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * times_table - prints the 9 times table starting from 0
+ * Return: void
+ */
+void times_table(void)
+{
+int next_result;
+int i = 0;
+while (i < 10){
+int j = 0;
+while (j < 10)
+{
+int re = i * j;
+next_result = i * (j+1);
+if (re > 9){
+putchar( (re / 10) + '0');
+putchar((re % 10) + '0');
+}
+else{
+putchar(re + '0');
+}
+if (j != 9)
+{
+if (next_result > 9){
+putchar(',');
+putchar(' ');
+}
+else{
+putchar(',');
+putchar(' ');
+putchar(' ');
+}
+}
+j++;
+}
+putchar('\n');
+i++;
+}
+}
