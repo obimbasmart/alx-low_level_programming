328b10a1ca9e6d53a358bcc2e40d2a0e6fd5468d
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-25 10:02:23 -0700

    added new file for fizzbuzz task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - fizBuzz code task
+ * Return: int : 0
+ */
+int main(void)
+{
+int num;
+num = 1;
+while (num < 101)
+{
+if (num % 3 == 0 && num % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (num % 3 == 0)
+{
+printf("Fizz");
+}
+else if (num % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", num);
+}
+if (num != 100)
+{
+putchar('-');
+}
+num++;
+}
+putchar('\n');
+}
