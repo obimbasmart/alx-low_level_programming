d459ff83393aec7a6cc5d99d17de48e8b4d9d0a4
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-25 08:00:08 -0700

    added new file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * print_line - draw a straight line in terminal
+ * @n : the number of _ chars to print
+ * Return: void
+ */
+void print_line(int n)
+{
+int i;
+i = 0;
+while (i < n)
+{
+if (n <= 0)
+{
+break;
+}
+putchar('_');
+i++;
+}
+putchar('\n');
+}
