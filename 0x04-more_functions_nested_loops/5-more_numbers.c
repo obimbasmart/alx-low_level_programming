2b94c776af94906e37bfc11cdb6b7b57e276607f
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-25 07:32:16 -0700

    added new file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * more_numbers - print the numbers 0 -14
+ * from 0 to 14, followed by a new line.
+ * Return: void
+ */
+void more_numbers(void)
+{
+int i;
+int num;
+i = 0;
+while (i < 10)
+{
+for (num = 0; num < 15; num++)
+{
+if (num > 9)
+{
+putchar((num / 10) + '0');
+putchar((num % 10) + '0');
+continue;
+}
+putchar(num + '0');
+}
+putchar('\n');
+i++;
+}
+}
