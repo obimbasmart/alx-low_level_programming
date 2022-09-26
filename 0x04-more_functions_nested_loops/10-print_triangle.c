2c0c19cb671ea318c9b4febc6ff034989665e6ad
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-26 13:23:17 -0700

    new task

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * print_triangle - prints a triangle
+ * @size : size of the triangle
+ * Return: void
+ */
+void print_triangle(int size)
+{
+int hashCounter, spaceCounter;
+hashCounter = 1;
+spaceCounter = size;
+int space, hash;
+if (size <= 0)
+{
+putchar('\n');
+}
+while (hashCounter <= size && spaceCounter > 0)
+{
+space = 1;
+hash = 0;
+while (space < spaceCounter)
+{
+putchar(' ');
+space++;
+}
+while (hash < hashCounter)
+{
+putchar('#');
+hash++;
+}
+hashCounter++;
+spaceCounter--;
+putchar('\n');
+}
+}
