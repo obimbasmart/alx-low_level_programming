19e36f7d71c50a6275260ce89e7d668357321d37
Obimba Smart <obimbasmartchukwunenye@gmail.com>
2022-09-26 22:59:21 +0100

    added all function to one file
    I moved all sub routines into one file due to checks failing on undefined reference to functions

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,3 +5,45 @@ int main(void)
 printf("%lu\n", get_highest_prime_factor(612852475143));
 return (0);
 }
+
+/**
+ * get_highest_prime_factor - that's it
+ * @num : the number to get its ...
+ * Return: int
+ */
+unsigned long int get_highest_prime_factor(unsigned long int num)
+{
+unsigned long int highest_prime_factor;
+unsigned long int n;
+n = 1;
+while (n < num)
+{
+if (num % n == 0 && _isprime(n))
+{
+highest_prime_factor = n;
+}
+n++;
+}
+return (highest_prime_factor);
+}
+
+/**
+ * _isprime - check if a number is prime
+ * @num : the number
+ * Return: 1 if true; 0 if false
+ */
+int _isprime(unsigned long int num)
+{
+unsigned long int factorCount;
+unsigned long int n;
+factorCount = 2, n = 2;
+while (n < num)
+{
+if (num % n == 0)
+{
+factorCount++;
+}
+n++;
+}
+return (factorCount == 2);
+}
