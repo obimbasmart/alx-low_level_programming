2845bc7493fa8c978e99d4260d33e88b83108973
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-27 06:15:46 -0700

    added new task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+/**
+ * swap_int - swap the value of two ints
+ * @a : the first num
+ * @b : the second num
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+int temp;
+temp = *a;
+*a = *b;
+*b = temp;
+}
