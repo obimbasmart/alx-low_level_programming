f9bfc0ba3e68120f8c5529fdbb353f8a9884e0e4
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-27 06:59:38 -0700

    added new task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+/**
+ * _strlen - return the lenght of a string
+ * @str : the string
+ * Return: integer
+ */
+int _strlen(char *str)
+{
+unsigned int index_count;
+index_count = 0;
+while (str[index_count] != '\0')
+{
+index_count++;
+}
+return index_count;
+}
