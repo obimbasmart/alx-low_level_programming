4e7a73622c87ac7c9e3898dadafacdebecceb4a1
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-27 14:03:11 -0700

    added new task

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * _strcpy - copy string from source to destination
+ * @src : the string
+ * @dest : the destination
+ * Return: pointer to destination
+ */
+void _strcpy(char *dest, char *src)
+{
+unsigned int idx;
+idx = 0;
+while (src[idx] != '\0')
+{
+dest[idx] = src[idx];
+idx++;
+}
+return (dest);
+}
+
+
