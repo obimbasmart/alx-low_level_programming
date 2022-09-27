e52d8078cf0639646569923de8461d4b60d9fb04
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-27 07:20:20 -0700

    added new task

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * _puts - print a string to sdout
+ * @str : the string
+ * Return: void
+ */
+void _puts(char *str)
+{
+unsigned int index_count;
+index_count = 0;
+while (str[index_count] != '\0')
+{
+putchar(str[index_count]);
+}
+putchar('n');
+}
