69f8bf113bb5b194ec9534dd83663f4b64a0567d
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-30 01:38:21 -0700

    added new task

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+/**
+ * _strncpy - copy string from source to destination
+ * @src : the source
+ * @dest : the destination
+ * @n : number of bytes to copy
+ * Return: pointer to destination
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int idx;
+idx = 0;
+while (idx < n && src[idx] != '\0')
+{
+dest[idx] = src[idx];
+idx++;
+}
+dest[idx] = '\0';
+return (dest);
+}
+
