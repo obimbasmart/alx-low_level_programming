b44770d6af172c8fc4072ad7e43b60f6f0d57ce9
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-30 05:15:18 -0700

    added new file

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * cap_string - convert all lowercase to uppercase
+ * @str : the string
+ * Return: pointer to @str
+ */
+char *cap_string(char *str)
+{
+int idx;
+idx = 1;
+while (str[idx] != '\0')
+{
+if (str[idx] > 96 && str[idx] < 123)
+{
+if (is_seperator(str[idx - 1]))
+{
+str[idx] = str[idx] - 32;
+}
+}
+idx++;
+}
+str[idx] = '\0';
+return (str);
+}
+/**
+ * is_seperator - check if a char is a valid seperator
+ * @ch : the character
+ * Return: 0:false, 1:true
+ */
+int is_seperator(int ch)
+{
+if (ch == '.' ||
+ch == ';' ||
+ch == ',' ||
+ch == '!' ||
+ch == '?' ||
+ch == '"' ||
+ch == '(' ||
+ch == ')' ||
+ch == '{' ||
+ch == '}' ||
+ch == ' ' ||
+ch == '\t' ||
+ch == '\n')
+{
+return (1);
+}
+return (0);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,4 +4,6 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
+int is_seperator(int ch);
+char *cap_string(char *);
 
