9bfb858b01af646fd036c3d73f59f71ab52e6153
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-30 01:18:12 -0700

    added new task

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+/**
+ * _strncat - concatenate two strings
+ * @src : the string to concatenate with
+ * @dest : the destination the resulting string
+ * @n : the number of bytes to concatenate from src
+ * Return: pointer to destination
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+unsigned int id_dest, id_src;
+char *temp_string[] = {};
+id_dest = 0;
+while (dest[id_dest] != '\0')
+{
+id_dest++;
+}
+id_src = 0;
+while (id_src < n)
+{
+dest[id_dest + id_src] = src[id_src];
+id_src++;
+}
+dest[id_dest + id_src] = '\0';
+return (dest);
+}
