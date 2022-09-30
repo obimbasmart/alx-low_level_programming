756a05216410124a93373afa2df6efb82efb9207
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-09-30 01:02:32 -0700

    initial commit
    added new task

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+/**
+ * _strcat - concatenate two strings
+ * @src : the string to concatenate with
+ * @dest : the destination the resulting string
+ * Return: pointer to destination
+ */
+char *_strcat(char *dest, char *src)
+{
+unsigned int id_dest, id_src;
+char *temp_string[] = {};
+id_dest = 0;
+while (dest[id_dest] != '\0')
+{
+id_dest++;
+}
+id_src = 0;
+while (src[id_src] != '\0')
+{
+dest[id_dest + id_src] = src[id_src];
+id_src++;
+}
+dest[id_dest + id_src] = '\0';
+return (dest);
+}
