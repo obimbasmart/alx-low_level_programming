b3610f538bda05ed44ca2720c56a360447743816
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-10-02 14:50:23 -0700

    added new task

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_number -  function that prints an integer
+ * @num : the integer
+ * Return: void
+ */
+void print_number(int num)
+{
+int n_digit, idx;
+n_digit = getdigitLength(num);
+idx = n_digit - 1;
+if (num < 0)
+{
+num = num * -1;
+putchar('-');
+}
+while (idx >= 0)
+{
+putchar((num / power(10, idx)) + '0');
+num = num % power(10, idx);
+idx--;
+}
+}
+/**
+ * getdigitLength -  function that returns the number of digits
+ * in an integer
+ * @num : the integer
+ * Return: integer
+ */
+int getdigitLength(int num)
+{
+int idx,  n_digit;
+n_digit = 0;
+idx = 0;
+while (num != 0)
+{
+num = num / 10;
+n_digit++;
+}
+return (n_digit);
+}
+/**
+ * power -  function that returns the power
+ * @base: the base
+ * @exp: the index
+ * Return: integer
+ */
+int power(int base, int exp)
+{
+int power, idx;
+idx = 0;
+power = 1;
+while (idx < exp)
+{
+power = power *base;
+idx++;
+}
+return (power);
+}
