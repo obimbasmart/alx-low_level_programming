2b1e94d70ca4ef3161d4a8f3dd91e674f140fbd7
Obimba Smart <obimbasmartchukwunenye@gmail.com>
2022-10-03 13:52:47 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,22 +6,19 @@
  */
 char *_strchr(char *s, char c)
 {
-char *ptr;
 unsigned int idx;
 idx = 0;
 while (s[idx] != '\0')
 {
 if (s[idx] == c)
 {
-ptr = &s[idx];
-return (ptr);
+return (s + idx);
 }
 idx++;
 }
 if (s[idx] == '\0')
 {
-ptr = '\0';
-return (ptr);
+return (s + idx);
 }
-return (0);
+return ('\0');
 }
