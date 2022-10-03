5fdf03a6dc3a79836a77196b1b10dcf1f9a8a82e
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-10-03 05:10:37 -0700

    added new task

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+/**
+ * _strchr - locates a character in a string
+ * @s: the string
+ * @c: the character
+ * Return: pointer to @c
+ */
+char *_strchr(char *s, char c)
+{
+char *ptr;
+unsigned int idx;
+idx = 0;
+while (s[idx] != '\0')
+{
+if (s[idx] == c)
+{
+ptr = &s[idx];
+return (ptr);
+}
+idx++;
+}
+ptr = 0;
+return (ptr);
+}
