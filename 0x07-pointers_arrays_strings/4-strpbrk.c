804830289a68aadd287591eaf990e7d84986c396
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-10-03 21:23:57 -0700

    added new task

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+/**
+ * _strpbrk - searches a string for any of a set of bytes.
+ * @s : the string to scan
+ * @accept : the search term
+ * Return: pointer to first @accept of accept in @s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int idx, idxx, *ptr;
+idx = 0;
+ptr = 0;
+while (s[idx] != '\0')
+
+{
+idxx = 0;
+while (accept[idxx] != '\0')
+{
+if (s[idx] == accept[idxx])
+{
+ptr = &s[idx];
+return (ptr);
+}
+idxx++;
+}
+idx++;
+}
+return (ptr);
+}
+
+
