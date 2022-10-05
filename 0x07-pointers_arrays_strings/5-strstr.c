0f1d3bafe921bb9b5b84ab45e948dad9e30f34b9
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-10-04 21:22:22 -0700

    added new task

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,38 @@
+/**
+ * _strstr - finds the first occurrence of the substring
+ * needle in the string haystack.
+ * @haystack: the string to scan
+ * @needle : the search term
+ * Return: pointer to first @accept of accept in @s
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int idx, idxx, is_substring, *ptr;
+idx = 0;
+ptr = 0;
+is_substring = 0;
+while (haystack[idx] != '\0')
+{
+idxx = 0;
+while (needle[idxx] != '\0')
+{
+if (needle[idxx] != haystack[idx + idxx])
+{
+is_substring = 0;
+break;
+}
+else
+{
+is_substring = 1;
+}
+idxx++;
+}
+if (is_substring)
+{
+ptr = &haystack[idx];
+return (ptr);
+}
+idx++;
+}
+return (ptr);
+}
