b2d19b555d5705d34834651a6af5fb76fb4b9215
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-10-08 21:44:13 -0700

    added new task

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+/**
+ * _sqrt_recursion - get the square root of a number
+ * @n: the number
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+if (n == 1 || n == 0)
+{
+return (n);
+}
+return (_get_sqr_root(n, 1));
+}
+
+/**
+ * _get_sqr_root - get the square root of a number with guess
+ * @n: the number
+ * @guess: the guess
+ * Return: int
+ */
+int _get_sqr_root(int n, int guess)
+{
+if (guess == n / 2)
+{
+return (-1);
+}
+if (guess * guess == n)
+{
+return (guess);
+}
+return (_get_sqr_root(n, guess + 1));
+}
