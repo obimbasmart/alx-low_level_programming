2e775106f976fff81d8656edb83c7d1619e9a5d8
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-10-08 20:37:38 -0700

    added new task

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+/**
+ * factorial - get factorial of a number
+ * @n: the number
+ * Return: int
+ */
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+if (n == 0)
+{
+return (1);
+}
+return (n * factorial(n - 1));
+}
+
