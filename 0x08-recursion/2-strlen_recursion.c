7e6bcbf11b64b6d25525ac08bf5e4752da226a4a
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-10-08 20:29:51 -0700

    added new task

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+/**
+ * _strlen_recursion - get the length of a string
+ * @s: the string
+ * Return: int
+ */
+unsigned int _strlen_recursion(char *s)
+{
+unsigned int idx;
+idx = 0;
+if (s[idx] ==  '\0')
+{
+return (0);
+}
+return (1 + _strlen_recursion(&s[idx + 1]));
+}
+
+
