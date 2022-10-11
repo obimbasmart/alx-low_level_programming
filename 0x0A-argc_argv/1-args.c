b9eee57af5d57e02b2b3d96a8fd30dded978413a
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-10-10 22:58:33 -0700

    added new task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the number of command line argument
+ * passed to a programm
+ * @argc: the command line argument count
+ * @argv: the command line argument vector
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc  - 1);
+return (0);
+}
