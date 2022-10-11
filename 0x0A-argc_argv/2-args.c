fc721ac8043b5e87587052bcd34816f8841125c4
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-10-10 23:15:47 -0700

    added new task

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print all the argument passed to a program
+ * @argc: the command line argument count
+ * @argv: the command line argument vector
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+int i;
+i = 0;
+while (i < argc)
+{
+printf("%s\n", argv[i]);
+i++;
+}
+return (0);
+}
