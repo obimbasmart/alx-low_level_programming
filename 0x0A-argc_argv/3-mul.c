9d169e21e480012a81038e806f390bcd88a4816e
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-10-11 00:18:43 -0700

    added new task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - print all the argument passed to a program
+ * @argc: the command line argument count
+ * @argv: the command line argument vector
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+int sum, i;
+sum = 0;
+i = 1;
+char *check;
+if (argc == 1)
+{
+printf("%d\n", sum);
+return (0);
+}
+while (i < argc)
+{
+check = argv[i];
+if (_isnumber(argv[i]))
+{
+sum += atoi(argv[i]);
+}
+else
+{
+printf("Error\n");
+return (0);
+}
+i++;
+}
+printf("%d\n", sum);
+return (0);
+}
+
+/**
+ * _isnumber - check if a string is a number
+ * @s: the string
+ * Return: 0 if false, 1 if true
+ */
+int _isnumber(char *s)
+{
+int i;
+for (i = 0; s[i]!= '\0'; i++)
+{
+if (isdigit(s[i]) == 0)
+return 0;
+}
+return 1;
+}
