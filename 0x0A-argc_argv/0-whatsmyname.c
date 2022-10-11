04467e95b138a1c7e5f0e0f454e0e9b5ec1a27a8
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-10-10 22:48:49 -0700

    initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print program name
+ * @argc: the command line argument count
+ * @argv: the command line argument vector
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
+}
