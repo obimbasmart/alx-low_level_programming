d2a81ded13cecde3903293f432bdf8a58a0b4654
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-10-13 12:36:13 -0700

    added new task

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+/**
+ * str_concat -  function that returns a pointer to a
+ * 2 dimensional array of integers
+ * @width: width of array
+ * @height: height of array
+ * Return: pointer;
+ */
+int **alloc_grid(int width, int height)
+{
+if (width <= 0 || height <= 0)
+{
+return (NULL);
+}
+int **arr;
+int w, h;
+arr =  malloc(sizeof(int **) * height);
+if (arr == NULL)
+{
+return (NULL);
+}
+h = 0;
+while (h < height)
+{
+arr[h] = malloc(width * sizeof(int *));
+h++;
+}
+h = 0;
+while (h < height)
+{
+w = 0;
+while (w < width)
+{
+arr[h][w] = 0;
+w++;
+}
+h++;
+}
+return (arr);
+}
