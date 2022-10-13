1d3f6dd25e2062916300431af8e8adfb2a03be08
Obimba Smart <obimbasmartchukwunenye@gmail.com>
2022-10-13 20:52:47 +0100

    fixed betty doc-style warning

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 /**
- * str_concat -  function that returns a pointer to a
+ * alloc_grid -  function that returns a pointer to a
  * 2 dimensional array of integers
  * @width: width of array
  * @height: height of array
