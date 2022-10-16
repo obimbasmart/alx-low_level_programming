59c203427a9e81c616543e9e5ff08ce26c5a9653
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-10-16 02:26:57 -0700

    formatted source code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,15 +5,15 @@
  */
 int main(void)
 {
-char theChar;
-int theInt;
-long int longInt;
-long long int longLongInt;
-float thefloat;
-printf("Size of a char: %d byte(s)\n", sizeof(theChar));
-printf("Size of an int: %d byte(s)\n", sizeof(theInt));
-printf("Size of a long int: %ld byte(s)\n", sizeof(longInt));
-printf("Size of a long long int: %d byte(s)\n", sizeof(longLongInt));
-printf("Size of a float: %d byte(s)\n", sizeof(thefloat));
-return (0);
+    char theChar;
+    int theInt;
+    long int longInt;
+    long long int longLongInt;
+    float thefloat;
+    printf("Size of a char: %d byte(s)\n", sizeof(theChar));
+    printf("Size of an int: %d byte(s)\n", sizeof(theInt));
+    printf("Size of a long int: %ld byte(s)\n", sizeof(longInt));
+    printf("Size of a long long int: %d byte(s)\n", sizeof(longLongInt));
+    printf("Size of a float: %d byte(s)\n", sizeof(thefloat));
+    return (0);
 }
