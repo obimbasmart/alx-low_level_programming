09cfba6f4bfefdc000e5388f67552a99c3cfd015
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-10-16 02:14:48 -0700

    formatted source

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,40 +9,40 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *s1_s2;
-unsigned int len1, len2, i, j;
-if (s1 == NULL)
-{
-s1 = "";
-}
-if (s2 == NULL)
-{
-s2 = "";
-}
-len1 = strlen(s1);
-len2 = strlen(s2);
-if (n < len2)
-{
-len2 = n;
-}
-s1_s2 = malloc(sizeof(char) * (len1 + len2 + 1));
-if (s1_s2 == NULL)
-{
-return (NULL);
-}
-i = 0;
-while (s1[i] != '\0')
-{
-s1_s2[i] = s1[i];
-i++;
-}
-j = i;
-i = 0;
-while (i < len2)
-{
-s1_s2[i + j] = s2[i];
-i++;
-}
-s1_s2[i + j] = '\0';
-return (s1_s2);
+    char *s1_s2;
+    unsigned int len1, len2, i, j;
+    if (s1 == NULL)
+    {
+        s1 = "";
+    }
+    if (s2 == NULL)
+    {
+        s2 = "";
+    }
+    len1 = strlen(s1);
+    len2 = strlen(s2);
+    if (n < len2)
+    {
+        len2 = n;
+    }
+    s1_s2 = malloc(sizeof(char) * (len1 + len2 + 1));
+    if (s1_s2 == NULL)
+    {
+        return (NULL);
+    }
+    i = 0;
+    while (s1[i] != '\0')
+    {
+        s1_s2[i] = s1[i];
+        i++;
+    }
+    j = i;
+    i = 0;
+    while (i < len2)
+    {
+        s1_s2[i + j] = s2[i];
+        i++;
+    }
+    s1_s2[i + j] = '\0';
+    return (s1_s2);
 }
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,22 +8,22 @@
  */
 int *array_range(int min, int max)
 {
-int *arr_ptr, i;
-if (min > max)
-{
-return (NULL);
-}
-arr_ptr = malloc((max - min + 1) * sizeof(int));
-if (arr_ptr == NULL)
-{
-return (NULL);
-}
-i = 0;
-while (min <= max)
-{
-arr_ptr[i] = min;
-min++;
-i++;
-}
-return (arr_ptr);
+    int *arr_ptr, i;
+    if (min > max)
+    {
+        return (NULL);
+    }
+    arr_ptr = malloc((max - min + 1) * sizeof(int));
+    if (arr_ptr == NULL)
+    {
+        return (NULL);
+    }
+    i = 0;
+    while (min <= max)
+    {
+        arr_ptr[i] = min;
+        min++;
+        i++;
+    }
+    return (arr_ptr);
 }
