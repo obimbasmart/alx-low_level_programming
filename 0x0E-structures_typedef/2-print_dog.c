e4ad01740e622154e13f6e664bd7b4f4b014570d
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-10-17 01:50:23 -0700

    initial commit

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * print_dog - prints a struct dog
+ * @d: the dog to print
+ */
+void print_dog(struct dog *d)
+{
+	printf(d == NULL ? "Name: (nil)\n" : "Name: %s\n", d->name);
+	printf(d == NULL ? "Age: (nil)\n" : "Age: %.6f\n", d->age);
+	printf(d == NULL ? "Owner: (nil)\n" : "Owner: %s\n", d->owner);
+}
+
