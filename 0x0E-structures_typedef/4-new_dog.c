c69a05042d16f9e30b5fb76e2b6c124f910c1116
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-10-17 14:02:49 -0700

    fixed compilation error

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,19 +15,31 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *dup_name;
 	char *dup_owner;
 
-	dup_name = malloc(sizeof(*name) * strlen(name));
-	dup_owner = malloc(sizeof(*owner) * strlen(owner));
-	strcpy(dup_name, name);
-	strcpy(dup_owner, owner);
-
 	new_dog_ = malloc(sizeof(struct dog));
 	if (new_dog_ == NULL)
 	{
 		return (NULL);
 	}
+
+	/* allocate size in memory for a copy of each string */
+	dup_name = malloc(sizeof(*name) * strlen(name));
+	dup_owner = malloc(sizeof(*owner) * strlen(owner));
+	if (dup_name == NULL || dup_owner == NULL)
+	{
+		free(dup_name);
+		free(dup_owner);
+		return (NULL);
+	}
+	/*copy the arguments into the newly created memory */
+	strcpy(dup_name, name);
+	strcpy(dup_owner, owner);
+
+	/* initialize the dog with the copy */
 	new_dog_->name = dup_name;
 	new_dog_->age = age;
 	new_dog_->owner = dup_owner;
+
+	/*free up memory */
 	return (new_dog_);
 }
 
