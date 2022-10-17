f97e8152fabb4599cdfe7c55485ba21bc3de60a9
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-10-17 01:05:15 -0700

    added new task

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - constructor to initialize a type: dog
+ * @d: the dog to initialize
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+void init_dog(struct dog *d, char *name,
+		float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
