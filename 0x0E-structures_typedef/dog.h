2fd4b26ee17904b50d9a4146aa5c0f01aaae16bc
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-10-17 00:53:38 -0700

    initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef dog_H
+#define dog_H
+
+/**
+ * struct dog - a simple structure for a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: onwer of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* end of dog_H */
+
