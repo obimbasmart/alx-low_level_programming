494cda664d85051378adf5ba65e49e0fa6629d5c
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-10-17 11:49:04 -0700

    fixed logical error - incorrect result

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,7 +10,7 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf(d->name == NULL ? "Name: (nil)\n" : "Name: %s\n", d->name);
-		printf(d->age == 0.0 ? "Age: (nil)\n" : "Age: %.6f\n", d->age);
+		printf(d->age == 0.0 ? "Age: 0.000000\n" : "Age: %.6f\n", d->age);
 		printf(d->owner == NULL ? "Owner: (nil)\n" : "Owner: %s\n", d->owner);
 	}
 }
