7726ba8d62fc99819cc30be51e480a39a6fd3d54
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-10-17 12:56:47 -0700

    added new task

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - functions that frees dog type from memory
+ * @d: the dog to free
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+}
+
