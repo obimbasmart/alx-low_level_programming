181d10f5bba4c8bb3c2cfcd79f404f95897f4536
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-10-18 14:48:29 -0700

    fixed segmentation fault

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,7 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int i;
 
 	/* check if function pointer is NULL */
-	if (action == NULL)
+	if (action == NULL || array == NULL)
 	{
 		return;
 	}
