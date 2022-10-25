0d736b0d97cd1330c7beb23e8e394fa020f7630f
Obimba Smart <obimbasmartchukwunenye@gmail.com>
2022-10-25 14:10:44 +0100

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -22,7 +22,7 @@ list_t *add_node(list_t **head, const char *str)
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
 	new_node->next = *head;
-	*head = new_node
+	*head = new_node;
 
 	return (new_node);
 }
