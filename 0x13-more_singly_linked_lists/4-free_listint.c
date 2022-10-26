f8349cf4e20560d3cdf504be705df446d6ffa6ec
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-10-26 03:46:12 -0700

    added new task

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_list - function frees a LL from mememory
+ * @head: head to LL
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+	{
+		return;
+	}
+	if (head->next == NULL)
+	{
+		free(head);
+		return;
+	}
+	free_listint(head->next);
+	free(head);
+}
+
