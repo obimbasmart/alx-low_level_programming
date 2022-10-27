d0701a7736df426e7cb287fd1711f11fe0180a12
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-10-27 14:03:39 -0700

    updted file

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -26,3 +26,29 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	}
 	return (head);
 }
+
+
+/**
+ * listint_len - return the number of elements in a linked listint_t
+ * @h: the head of the list
+ *
+ * Return: the number of nodes in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	unsigned int n_nodes;
+	n_nodes = 0;
+	if (h == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		do {
+			n_nodes++;
+			h = h->next;
+		} while (h != NULL);
+	}
+	return (n_nodes);
+
+}
