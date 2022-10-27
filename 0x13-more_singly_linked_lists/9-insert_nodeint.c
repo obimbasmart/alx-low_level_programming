bb3c63addd1b003a0e04ab8ea10c34c322143194
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-10-27 15:51:16 -0700

    modified file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,3 +28,56 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	return (new_node);
 }
+
+/**
+ * get_nodeint_at_index -  returns the nth node of a listint_t LL
+ * @head: head node/first node
+ * @index: the index of the index node starting at 0
+ *
+ * Return: nth_nod of listint_t
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int counter, n_nodes;
+
+	n_nodes = listint_len(head);
+	counter = 0;
+
+	if (index >= n_nodes)
+	{
+		return (NULL);
+	}
+
+	while (counter < index)
+	{
+		head = head->next;
+		counter++;
+	}
+	return (head);
+}
+
+
+/**
+ * listint_len - return the number of elements in a linked listint_t
+ * @h: the head of the list
+ *
+ * Return: the number of nodes in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	unsigned int n_nodes;
+
+	n_nodes = 0;
+	if (h == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		do {
+			n_nodes++;
+			h = h->next;
+		} while (h != NULL);
+	}
+	return (n_nodes);
+}
