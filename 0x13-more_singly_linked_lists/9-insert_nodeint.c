9261a4a8efc452ed4df98c1a6dc9ad994f503ab3
Obimba Smart <obimbasmartchukwunenye@gmail.com>
2022-10-29 00:04:44 +0100

    fixed logical error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 	/* if the linked list is empty */
-	if (back_node == NULL && idx == 0)
+	if (idx == 0)
 	{
 		return (add_nodeint(head, n));
 	}
