62ac429ac7c45b56c819aa93cbdeda58de8efedf
Obimba Smart <obimbasmartchukwunenye@gmail.com>
2022-10-29 00:28:58 +0100

    updated code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,6 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (add_nodeint_end(head, n));
 	}
 	
+	idx--;
 	while (idx != 0)
 	{
 		back_node = back_node->next;
