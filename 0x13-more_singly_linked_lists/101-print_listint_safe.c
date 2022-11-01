ff609ed65dee9d522192c7bd216c26faab5a9e41
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-11-01 10:07:45 +0000

    updated algorithm and code

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,127 +1,34 @@
-#include<stdio.h>
 #include "lists.h"
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * print_listint_safe - handle both cyclic LL
- * @head: pointer for first node
+ * print_listint_safe - print a linked list
+ * @head: pointer to first node in list
  *
- * Return: size of linked list
+ * Return: number of nodes printed
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	listint_t *junction;
-	unsigned int list_len;
-
-	list_len = 0;
-	junction = (listint_t *) find_junction(head);
-	if (junction)
+	unsigned int len;
+	int diff;
+	
+	len = 0;
+	if (!head)
 	{
-		if (junction == head)
+		return (len);
+	}
+	while (head)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		diff = head - head->next;
+		len += 1;
+		if (diff <= 0)
 		{
-			list_len += print_cycle(junction);
+			
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
 		}
-		else
-		{
-			while (head != junction)
-			{
-				printf("[%p] %d\n", (void *) head, head->n);
-				list_len += 1;
-				head = head->next;
-			}
-			list_len += print_cycle(junction);
-		}
+		head = head->next;
 	}
-
-	else
-	{
-		list_len += print_listint(head);
-	}
-	return (list_len);
+	return (len);
 }
-
-/**
- * find_junction - find the junction node of a cyclic LL
- * @head: pointer to first node
- *
- * Return: the junction node
- */
-listint_t *find_junction(const listint_t *head)
-{
-	listint_t *fastptr, *slowptr;
-
-	slowptr = fastptr = (listint_t *) head;
-	/* find the point where both fastptr and slowptr meet */
-	while (fastptr && fastptr->next)
-	{
-		slowptr = slowptr->next;
-		fastptr = fastptr->next;
-		if (fastptr)
-		{
-			fastptr = fastptr->next;
-		}
-		if (fastptr == slowptr)
-		{
-			slowptr = (listint_t *)head;
-			while (fastptr != slowptr)
-			{
-				slowptr = slowptr->next;
-				fastptr = fastptr->next;
-			}
-			return (fastptr);
-		}
-	}
-	return (NULL);
-}
-
-/**
- * print_cycle - print a cyclic linked list
- * @junction: where to start and stop
- *
- * Return: lenght of linked list
- */
-size_t print_cycle(listint_t *junction)
-{
-	int cycle_len;
-	listint_t *traverse;
-
-	traverse = junction;
-	cycle_len = 0;
-
-	do {
-		printf("[%p] %d\n", (void *)traverse, traverse->n);
-		cycle_len++;
-		traverse = traverse->next;
-	} while (traverse != junction);
-	printf("-> [%p] %d\n", (void *) junction, junction->n);
-	return (cycle_len);
-}
-
-/**
- * print_listint - prints all the elements of a list_t list
- * @h: the head of the list
- *
- * Return: the number of nodes in the list
- */
-size_t print_listint(const listint_t *h)
-{
-	unsigned int n_nodes;
-
-	n_nodes = 0;
-	if (h == NULL)
-	{
-		return (0);
-	}
-
-	else
-	{
-		do {
-			printf("[%p] %d\n", (void *)h, h->n);
-			n_nodes++;
-			h = h->next;
-		} while (h != NULL);
-	}
-	return (n_nodes);
-}
-
-
