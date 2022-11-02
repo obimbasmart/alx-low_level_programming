48a7ad177c518cba45177f7d8dc8f682a495baf3
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-11-02 00:00:29 +0000

    initial commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * binary_to_uint - converts a binary to an unsigned int
+ * @b: string of 0 and 1 chars
+ *
+ * Return: converted number or 0 if NULL or invalid input
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num, len, idx;
+
+	len = strlen(b);
+	num = idx = 0;
+
+	while (len--)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+		num += (b[len] - '0') * power(2, idx);
+		idx++;
+	}
+	return (num);
+}
+
+/**
+ * power - get the power of a number raised by exp
+ * @exp: the exponent
+ * @base: the base number
+ *
+ * Return: power raised to exp : integer
+ */
+unsigned int power(unsigned int base, unsigned int exp)
+{
+	unsigned int pow;
+
+	pow = 1;
+	if (exp == 0)
+	{
+		return (pow);
+	}
+
+	while (exp--)
+	{
+		pow *= base;
+	}
+	return (pow);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,6 @@
+#ifndef main_h
+#define main_h
+
+unsigned int binary_to_uint(const char *b);
+unsigned int power(unsigned int base, unsigned int exp);
+#endif /* end of main_h */
