352b15d309922752fa7e4dd27320e5afdc4e4405
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-11-05 02:22:46 +0000

    added new task

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,4 +6,7 @@ unsigned int power(unsigned int base, unsigned int exp);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+
+
 #endif /* end of main_h */
