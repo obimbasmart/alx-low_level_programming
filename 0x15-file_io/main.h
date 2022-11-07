14c21acd322762faa722968906684295a7a0b527
Obimba Smart <obimbasmartchukwunenye@gmail.com>
2022-11-07 16:18:36 +0100

    refactoring

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,5 +12,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void copy_content(int , int, int, char *, char *);
+void print_to_stderr(char *, char *, unsigned int);
 
 #endif /* end of main_h */
