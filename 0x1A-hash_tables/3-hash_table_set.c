a1fb83dea03d600a81beceda4e08240da56a8002
Obimba Smart <obimbasmartchukwunenye@gmail.com>
2023-01-11 00:36:06 +0100

    store copy of key/value

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,7 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!new_hash_node)
 		return (0);
 
-	new_hash_node->key = (char *) key;
+	new_hash_node->key = strdup((char *) key);
 	new_hash_node->value = strdup(value);
 	new_hash_node->next = NULL;
 
