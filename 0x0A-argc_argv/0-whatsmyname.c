adc76e75590845d14e465c97fd15f88a30a17311
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-06-10 14:47:19 +0000

    initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - driver program: prints program name
+ * @argc: integer
+ * @argv: array of args
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (EXIT_SUCCESS);
+}
