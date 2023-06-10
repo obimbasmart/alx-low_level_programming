107d334de375bc19d9c8c81f7ed4679c287a6471
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-06-10 14:52:06 +0000

    suppressed warning due to unsuded params

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  * @argv: array of args
  * Return: int
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 	return (EXIT_SUCCESS);
