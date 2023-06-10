018d7186a7b7fffedb9fa98323d8d1a94bf2dfc5
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-06-10 15:01:11 +0000

    added new task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - driver program: prints program name
+ * @argc: lenght of argv
+ * @argv: array of args
+ * Return: int
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (EXIT_SUCCESS);
+}
