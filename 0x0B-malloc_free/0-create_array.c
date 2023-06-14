e955d554a33f10b808da7a074d60c192550b8426
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-06-14 04:15:04 +0000

    initial commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars, and initialize
+ * @size: integer size of array
+ * @c: char init
+ * Return: pointer to array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	size_t idx;
+
+	arr = malloc(sizeof(char) * size);
+	if (!arr)
+		return (NULL);
+
+	for (idx = 0; idx < size; idx++)
+		arr[idx] = c;
+
+	return (arr);
+}
+
