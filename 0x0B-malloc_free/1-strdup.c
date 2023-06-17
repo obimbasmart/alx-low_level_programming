fc888edeecfd6e7c51103abe4e01d1a3ce2af9da
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-06-17 13:26:17 +0000

    fixed logical error

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,10 +10,13 @@ char *_strdup(char *str)
 	char *dup_str;
 	unsigned int size, idx;
 
+	if (!str)
+		return (NULL);
+
 	size = strlen(str);
 	dup_str = malloc(sizeof(char) * size + 1);
 
-	if (!dup_str || !*str)
+	if (!dup_str)
 		return (NULL);
 
 	idx = 0;
