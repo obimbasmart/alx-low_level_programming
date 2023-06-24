be82e2ca721478e00395a2759ae77d9f17c935cb
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-06-24 04:31:01 +0000

    added new task

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * array_rante - create an array of integers
+ * @min: mininmum integer start
+ * @max: maximum integer stop
+ * Return: pointer to array of integer
+ */
+int *array_range(int min, int max)
+{
+	int *array, id;
+
+	if (min > max)
+		return (NULL);
+
+	array = malloc(sizeof(int) * (max - min + 1));
+
+	if (!array)
+		return (NULL);
+
+	id = 0;
+	while (min <= max)
+	{
+		array[id] = min;
+		min++;
+	}
+}
