7cf192980343c66336bed7d02b017c3878888501
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-06-25 12:37:34 +0000

    added new task

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,9 @@
+# C - Structures, typedef
+
+## Mandatory
+
+0. Define a new type struct `dog` with the following elements:
+
+	- `name`, type = `char *`
+	- `age`, type = `float`
+	- `owner`, `type` = `char *`
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef HEADER_F
+#define HEADER_F
+
+/**
+ * struct dog - a typical dog with attributes
+ * @name: name of dog
+ * @age: how old the dog is
+ * @owner: who owns the dog
+ *
+ * Description: This is a simple blueprint that represents a dog
+ * with its specifica attributes unique to it
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* HEADER_H */
