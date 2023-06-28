9885dc7160f58c4c2cc4e52b486074b816a91454
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-06-28 06:52:37 +0000

    added new task

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_strings - prints strings, followed by a new line.
+ * @separator: number seperator
+ * @n: number of arguments passed
+ *
+ * Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int id;
+
+	va_start(args, n);
+	if (!separator)
+		separator = "(nil)";
+
+	id = 0;
+
+	while (id < n)
+	{
+		printf("%s", va_arg(args, char *));
+
+		/* if last number */
+		if (n - id != 1)
+			printf("%s", separator);
+
+		id++;
+	}
+	printf("\n");
+	va_end(args);
+}
+
+
