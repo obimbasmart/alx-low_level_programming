2a3d96a97e99b1e357786658340d7f8b51eec3e0
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-07-06 00:00:24 +0000

    added new task

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_len - return the number of elements in a linked listint_t
+ * @h: the head of the list
+ *
+ * Return: the number of nodes in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t n_nodes;
+
+	n_nodes = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h)
+	{
+			n_nodes++;
+			h = h->next;
+	}
+
+	return (n_nodes);
+}
