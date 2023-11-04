36a88a81c92a4723a500b0e94fe6aa5ee50b852a
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-11-04 02:20:23 +0100

    new: calc perimeter of island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""""calculate perimeter of an island grid"""
+
+
+def island_perimeter(grid):
+    """calculate the perimeter of an island grid"""
+    perimeter = 0
+    for x in range(5):
+        for y in range(6):
+            perimeter += get_perimeter(y, x, grid)
+
+    return perimeter
+
+def get_perimeter(y, x, grid):
+    """get perimeter of a single cell"""
+    perimeter = 0
+    idx = [(x - 1, y), (x, y + 1), (x + 1, y), (x, y - 1)]
+    if (grid[x][y]):
+        for id in idx:
+            if not grid[id[0]][id[1]]:
+                perimeter += 1
+    return (perimeter)
+
+
